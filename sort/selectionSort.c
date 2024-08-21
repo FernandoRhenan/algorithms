2aa2f17c01e779daf8e1154c8e9badebab7cdf59
FernandoRhenan <fernandorhenan9@gmail.com>
2024-08-21 19:30:49 -0300

    adds ´comments´ on selectionSort

--- sort/selectionSort.c
+++ sort/selectionSort.c
@@ -6,17 +6,20 @@ int main()
     int n = sizeof(array) / sizeof(array[0]);
     int i, j;
 
+    // n - 1 == o loop percorre até o penúltimo elemento.
     for (i = 0; i < n - 1; i++)
     {
         int smallest_index = i;
         for (j = i + 1; j < n; j++)
         {
+            // Procura o menor número de todo o array e atribui o seu index a smallest_index
             if (array[j] < array[smallest_index])
             {
                 smallest_index = j;
             }
         }
 
+        // Se o menor número ja não estiver na posição correta, ele o põe.
         if (smallest_index != i)
         {
             int aux = array[i];
@@ -25,7 +28,6 @@ int main()
         }
     }
 
-    // Just prints, this is not part of selection sort
     for (int x = 0; x < n; x++)
     {
         printf("%d, ", array[x]);
